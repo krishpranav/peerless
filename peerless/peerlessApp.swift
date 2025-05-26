0c532efdf5a3ebd7b1c4a58c2305cccd6a39f880
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-26 15:40:07 +0530

    add files via upload

---
+++ peerless/peerlessApp.swift
@@ -0,0 +1,17 @@
+//
+//  peerlessApp.swift
+//  peerless
+//
+//  Created by Krisna Pranav on 26/05/25.
+//
+
+import SwiftUI
+
+@main
+struct peerlessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
